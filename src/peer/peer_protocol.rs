a0765bc07ec0f7fe1528d783f4f15ba63661b9bc
tanndlin <tanndlin@gmail.com>
2026-09-24 17:40:51 -0400

    Check for pieces existing peers completed

--- src/peer/peer_protocol.rs
+++ src/peer/peer_protocol.rs
@@ -102,14 +102,19 @@ pub fn connect_to_peer(
         }
 
         // Choose 5 random pieces that the peer has and that we don't have
-        let needed_pieces: Vec<u32> = progress
-            .read()
-            .unwrap()
-            .needed_pieces
-            .iter()
-            .copied()
-            .filter(|&piece_index| bitfield_contains_piece(&peer_state.bitfield, piece_index))
-            .collect();
+        let needed_pieces: Vec<u32> = {
+            let progress = progress.read().unwrap();
+            // Remove the piece from requested pieces if another peer already completed it
+            peer_state
+                .requested_pieces
+                .retain(|piece_index| progress.needed_pieces.contains(piece_index));
+            progress
+                .needed_pieces
+                .iter()
+                .copied()
+                .filter(|&piece_index| bitfield_contains_piece(&peer_state.bitfield, piece_index))
+                .collect()
+        };
 
         while peer_state.requested_pieces.len() < 5 {
             if let Some(piece_index) = needed_pieces.choose(&mut rand) {
